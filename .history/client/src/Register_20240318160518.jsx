d900f0087f21e564581a9132803c831e051abe1e
HAONANTAO <873190199@qq.com>
2024-03-18 16:13:24 +1100

    add server side package.json

---
+++ .history/client/src/Register_20240318160518.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+
+const Register = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    // 全局居中
+    <div className="bg-blue-50 h-screen flex items-center">
+      {/* margin auto == center 左右居中*/}
+      <form className="w-64 mx-auto" onSubmit={handSubmit()}>
+        <input
+          type="text"
+          value={username}
+          placeholder="input username..."
+          className="block w-full rounded-sm p-2 mb-2"
+          onChange={(ev) => setUsername(ev.target.value)}></input>
+
+        <input
+          type="password"
+          value={password}
+          placeholder="input password..."
+          className="block w-full rounded-sm p-2 mb-2"
+          onChange={(ev) => setPassword(ev.target.value)}></input>
+        <button className="bg-blue-500 text-white block w-full rounded-sm">
+          Register
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Register;
